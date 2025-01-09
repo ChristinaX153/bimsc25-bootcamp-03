8d9538bb3ef6972d5041d54d39ba3adb2e6fd495
ChristinaX153 <christina.christoforou@students.iaac.net>
2025-01-09 18:41:15 +0200

    Create scriptCode.js

---
+++ scriptCode.js
@@ -0,0 +1,41 @@
+console.log('hello')
+
+const myConstVar = 5 
+
+
+let foo = 93
+console.log(foo)
+
+foo = "my string"
+
+let foodArray = ["pizza", "pasta", "salad"]
+console.log(foodArray)
+
+let foodItem = foodArrray[1]
+console.log("foodItem:", foodItem)
+
+let objectVariable = {
+    key: 'value',
+    foods: foodArray
+}
+console.log(objectVariable)
+
+let foods - objectVariable['food']
+console.logP('foods:', foods)
+
+function AddTwoNumbers(num1, num2){
+    let sum = num1 + num2
+    return sum
+}
+
+let mySum = AddTwoNumbers(6, 7)
+console.log('mySum:', mySum)
+
+let breakfastDiv = document.getElrmrntbyId("breakfast")
+console.log(breakfastElement)
+
+function breakfastClicked(){
+    console.log('breakfast element was clicked')
+}
+
+breakfastElement.addEventListener("click", () => breakfastClicked())
